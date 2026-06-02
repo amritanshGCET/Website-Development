e1ecaebae00eed59e84341f4956ac9111974eb5b
Amritansh singh <amritansh.25gcebec121@galgotiacollege.edu>
2026-06-02 16:05:05 +0530

    Second commit

--- JavaScript/practiseSET/Classes/staticprop.js
+++ JavaScript/practiseSET/Classes/staticprop.js
@@ -1,17 +1,17 @@
 class User {
-    constructor(username){
-        this.username=username;
+    constructor(username) {
+        this.username = username;
     }
 
-    logMe(){
+    logMe() {
         console.log(`Username: ${this.username}`)
     }
 
-   static crtId(){ //static avoids giving accses to the object instantiated by the class
-            return 123;
+    static crtId() {     //static avoids giving accses to the object instantiated by the class
+        return 123;
     }
 }
 
-const Amrit= new User("Amritansh singh");
+const Amrit = new User("Amritansh singh");
 Amrit.logMe();
 //console.log(Amrit.crtId()); //gives error that crtId is not a function
